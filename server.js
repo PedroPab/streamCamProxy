32ff81de2d0c7f30a2bc2f0fe57bf30351d2db2e
PedroPab <pmontesarroyave@gmail.com>
2025-12-14 17:37:30 -0500

    Actualizar configuración de Docker y ajustar el manejo del puerto en server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const ESP32_HOST = process.env.ESP32_HOST || '192.168.1.6';
 const ESP32_PORT = process.env.ESP32_PORT || 81;
 const ESP32_PATH = process.env.ESP32_PATH || '/stream';
-const PORT = 3001;
+const PORT_external = process.env.PORT || 3001;
 
 const ESP32_URL = `http://${ESP32_HOST}:${ESP32_PORT}${ESP32_PATH}`;
 console.log('URL de la ESP32-CAM:', ESP32_URL);
@@ -135,7 +135,7 @@ const server = http.createServer((req, res) => {
 });
 
 
-server.listen(PORT, () => {
-    console.log(`Servidor proxy escuchando en http://localhost:${PORT}/`);
-    console.log(`Prueba el stream en http://localhost:${PORT}/stream`);
+server.listen(3001, () => {
+    console.log(`Servidor proxy escuchando en http://localhost:${PORT_external}/`);
+    console.log(`Prueba el stream en http://localhost:${PORT_external}/stream`);
 });
